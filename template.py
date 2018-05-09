0048a93395ff14172e9e521a66382ce5dc0dca90
Kamil Karczewski <serwisyrejestracje@wp.pl>
2018-05-09 14:52:31 +0200

    excluding xml_logger, os_call to separate project
    Excluding xml_logger and os_call to separate project. This should be simple usage template. 
    Added comments, translations, small refactoring.

--- template.py
+++ template.py
@@ -18,12 +18,8 @@ import subprocess
 # #############################################################################
 # constants, global variables
 # #############################################################################
-OUTPUT_ENCODING = 'utf-8'
-DIRECTORY = './'
-TEMP_PATH = SCRIPT_DIR+'/cache/'
-LOGGER_PATH = SCRIPT_DIR+'/logfile.xml'
-LOG_VERSION = 1.0
 NAME = __file__
+TEMP_PATH = SCRIPT_DIR+'/cache/'
 SPLIT_DIR = os.path.dirname(os.path.realpath(NAME))
 SCRIPT_DIR = SPLIT_DIR + '/.' + os.path.basename(NAME)
 LIB_DIR = SCRIPT_DIR + '/cache/lib/'
@@ -110,106 +106,6 @@ def print_err(error):
 def print_war(warning):
    print(colorama.Fore.YELLOW+warning,colorama.Fore.RESET)
 
-# Pretty Print for xml log system (xml.etree don't support it, recommend to use lxml)
-# lxml will be used in next version
-def indent(elem, level=0):
-  i = "\n" + level*"  "
-  if len(elem):
-    if not elem.text or not elem.text.strip():
-      elem.text = i + "  "
-    if not elem.tail or not elem.tail.strip():
-      elem.tail = i
-    for elem in elem:
-      indent(elem, level+1)
-    if not elem.tail or not elem.tail.strip():
-      elem.tail = i
-  else:
-    if level and (not elem.tail or not elem.tail.strip()):
-      elem.tail = i
-
-#XML logging system 
-#Will be refactored to lxml in next version
-def my_logger(ERROR_FLAG,subcmd,outmsg):
-   import xml.etree.ElementTree as ET # in case of usege - move on the top of the file to the list of libs in use
-   import datetime # in case of usege - move on the top of the file to the list of libs in use
-   import base64 # in case of usege - move on the top of the file to the list of libs in use
-   
-   id_log = 1
-   if not os.path.exists(LOGGER_PATH):
-      root = ET.Element('root')
-      root.set('version','1.0')
-   else:
-      tree = ET.parse(LOGGER_PATH)
-      root = tree.getroot()
-      for child in root:
-         id_log+=1
-   log = ET.SubElement(root, 'log')
-   log.set('id_log',str(id_log))
-   date = ET.SubElement(log,'date')
-   date.text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S,%f')
-   cmdline = str()
-   for line in sys.argv:
-      cmdline += line+' '
-   command = ET.SubElement(log,'command')
-   command.set('encoding','plain')
-   command.text = cmdline
-   subcommands = ET.SubElement(log,'subcommands')
-   subcommands.set('error_flag',ERROR_FLAG)
-   subcmd_str=str()
-   for one in subcmd:
-      subcmd_str+=one+','
-   subcommands.text = subcmd_str[:-1]
-   outmsg_str = str()
-   for one in outmsg:
-      outmsg_str+=one+','
-   msg = (base64.b64encode(outmsg_str.encode(OUTPUT_ENCODING))).decode(OUTPUT_ENCODING)
-   output = ET.SubElement(log,'output')
-   output.set('encoding','base64')
-   output.text = msg
-   indent(root)
-   if not os.path.exists(LOGGER_PATH):
-      tree = ET.ElementTree(root)
-   tree.write(LOGGER_PATH,encoding=OUTPUT_ENCODING,xml_declaration=True,method='xml')
-
-#Executing bash commands in terminal from python script. Fancy method with asterix as progress bar.
-#TO DO!!! Simple os_call_simple(*args)
-def os_call(*args,progress_char='*',verbose=1):
-   import time # in case of usege - move on the top of the file to the list of libs in use
-   n = 0
-   done_cmd = list()
-   out = list()
-   for cmd in args:
-      p = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=DIRECTORY)
-      (output,err) = p.communicate()
-      n = n+1
-      ast = progress_char*n
-      if err or 'ERROR' in str(output) or 'Exception' in str(output):
-         done_cmd.append(cmd)
-         ERROR_FLAG = 'T'
-         print_err(cmd)
-         if err:
-            print_err(err.decode(OUTPUT_ENCODING))
-            out.append(err.decode(OUTPUT_ENCODING))
-            break
-         else:
-            print_err(output.decode(OUTPUT_ENCODING))
-            out.append(output.decode(OUTPUT_ENCODING))
-            break
-      else:
-         ERROR_FLAG = 'F'
-         done_cmd.append(cmd)
-         out.append(output.decode(OUTPUT_ENCODING))
-         if verbose == 2:
-            print(ast,end="\r")
-            time.sleep(1)
-            print_ok(cmd)
-            print_ok(output.decode(OUTPUT_ENCODING))
-         elif verbose == 1:
-            print_ok(output.decode(OUTPUT_ENCODING))
-         else:
-            print(ast,end='\r')
-   return ERROR_FLAG,done_cmd,out
-
 # Paramiko example
 def logonssh(server,loginssh,cmd):
    import getpass # in case of usege - move on the top of the file to the list of libs in use
@@ -288,6 +184,20 @@ def opt_db_engine(args):
    engine_text = temp.safe_substitute(config)
    if 'localhost' in engine_text:
       engine_text+='?unix_socket=/var/run/mysqld/mysqld.sock'
+def test():
+   # Testowe wydruki
+   lines = ['ls -l','mkdir test','ls -la','touch plik']
+   lines = readfile('sza.txt')
+   ERROR_FLAG,done_cmd,out = os_call(*lines,progress_char='*',verbose=2)
+   logonssh('dev.justnet.pl','kamil','ls -la')
+   csv_write('eggs.csv',' ')
+   csv_read('eggs.csv',' ')
+   opt_db_engine(args) # przenieść do if:elif: w miejscu w którym używamy bazy
+   query0 = text("SELECT COUNT(id) FROM history WHERE content NOT LIKE '%<object source=\"Client\">%' AND src LIKE 'DATASOURCE' AND table_name LIKE 'Client%'")
+   count = simple_query(query0, None)
+   for one in count:
+      for two in one:
+         print('Ilość rekordów: ', two)
 
 def opt_help():
    parser.print_help()
@@ -298,13 +208,17 @@ def opt_help():
 # #############################################################################
 # main app 
 # #############################################################################
-if __name__ == '__main__':
-# Reading arguments
-   try:
-      from sqlalchemy.sql import text
-   except Exception as e:
-      print_err('No sqlalchemy installation')
-      print_err(e)
+
+def main():
+   # Reading arguments
+   #Uncomment in case of usage db connection.
+   #try:
+   #   #Bug with sqlalchemy. Workaround After first installation it has to be imported twice.
+   #   #Don't know why, still under investigation.
+   #   from sqlalchemy.sql import text
+   #except Exception as e:
+   #   print_err('No sqlalchemy installation')
+   #   print_err(e)
    parser = argparse.ArgumentParser(
       prog='template.py',
       description='Description script',
@@ -343,34 +257,19 @@ write password in prompt.''')
       help='Description subone')
    argv = sys.argv[1:]
    args = parser.parse_args(argv)
-   optd = vars(args)
-   ctx  = dict()
    try:
-      #if not len(sys.argv) > 1 or 'help' in args:
-      #   opt_help()
+      if not len(sys.argv) > 1 or 'help' in args:
+         opt_help()
+      elif 'test' in args:
+         opt_test()
       #elif 'arg_position' in args:
       #   opt_position(ctx, args.arg_position)
       #elif 'arg_named' in args:
       #   opt_some(ctx, args.arg_named, 'none', -1)
       #elif 'sub-arg' in args:
       #   opt_some(ctx,args.sub-arg,'none',-1)      
-      #else:
-      #   opt_help()
-
-# Testowe wydruki
-#      lines = ['ls -l','mkdir test','ls -la','touch plik']
-#      lines = readfile('sza.txt')
-#      ERROR_FLAG,done_cmd,out = os_call(*lines,progress_char='*',verbose=2)
-#      my_logger(ERROR_FLAG,done_cmd,out)
-#      logonssh('dev.justnet.pl','kamil','ls -la')
-#      csv_write('eggs.csv',' ')
-#      csv_read('eggs.csv',' ')
-      opt_db_engine(args) # przenieść do if:elif: w miejscu w którym używamy bazy
-      query0 = text("SELECT COUNT(id) FROM history WHERE content NOT LIKE '%<object source=\"Client\">%' AND src LIKE 'DATASOURCE' AND table_name LIKE 'Client%'")
-      count = simple_query(query0, None)
-      for one in count:
-          for two in one:
-              print('Ilość rekordów: ', two)
+      else:
+         opt_help()
    except Exception as e:
       cmd = str()
       for one_arg in sys.argv:
@@ -380,3 +279,8 @@ write password in prompt.''')
       err_msg = str(e)
       my_logger('T',list_cmd,err_msg)
       print(e)
+   
+
+if __name__ == '__main__':
+   main()
+
